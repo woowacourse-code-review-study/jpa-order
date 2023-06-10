64a206de1b18e2cb485c595606a4329d4a926966
greeng00se <thegreengoose7@gmail.com>
2023-06-10 17:16:12 +0900

    feat: 초기 설정

---
+++ src/main/java/my/order/JpaOrderApplication.java
@@ -0,0 +1,13 @@
+package my.order;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaOrderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JpaOrderApplication.class, args);
+    }
+
+}
---
+++ src/test/java/my/order/JpaOrderApplicationTests.java
@@ -0,0 +1,13 @@
+package my.order;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaOrderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
